ff28eefdc73db1985df06964a40b21af6fbc8152
Siwoo Lee <siwoo28@kakao.com>
2023-11-17 19:26:09 +0900

    refactor: todoinfo 파일 변환

---
+++ app/_components/TodoInfo.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import React from "react";
+import TodoInfoItem from "@/app/_components/TodoInfoItem";
+
+async function TodoInfo(id) {
+  try {
+    console.log("check", id);
+    const res = await fetch(`http://localhost:33088/api/todoList/1`);
+    const data: TodoResponse = await res.json();
+
+    if (data) {
+      return (
+        <div>
+          <TodoInfoItem data={data} />
+        </div>
+      );
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+export default TodoInfo;
---
+++ app/_components/TodoInfoItem.tsx
@@ -0,0 +1,9 @@
+"use client";
+import React from "react";
+
+function TodoInfoItem(data) {
+  console.log(data.data.item);
+  return <div>TodoInfoItem Hi</div>;
+}
+
+export default TodoInfoItem;
